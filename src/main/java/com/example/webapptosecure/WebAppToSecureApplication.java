cdd4d2a51cff2156fcfd32ecac7d0830970d11db
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-12-30 11:24:52 +0200

    initial commit

---
+++ src/main/java/com/example/webapptosecure/WebAppToSecureApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapptosecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppToSecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppToSecureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webapptosecure/WebAppToSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapptosecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppToSecureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
